71633e74d544f06d892755768c555d5a39b8d86f
bculberson <brad.culberson@sendgrid.com>
2016-06-09 11:12:35 -0600

    just an example of using aws s3 lib to send/recv bytes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # go-s3-example
+
+Requirements to run:
+
+export AWS_ACCESS_KEY_ID=<your access key id>
+export AWS_SECRET_ACCESS_KEY=<your secret access key>
+export AWS_REGION=<your region>
+export AWS_BUCKET=<your bucket>
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func testEq(a, b []byte) bool {
+
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func send(key string, b []byte) error {
+	bucketName := os.Getenv("AWS_BUCKET")
+	svc := s3.New(session.New(), aws.NewConfig())
+	_, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(b),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func recv(key string) ([]byte, error) {
+	bucketName := os.Getenv("AWS_BUCKET")
+	svc := s3.New(session.New(), aws.NewConfig())
+	output, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return nil, err
+	}
+	result, err := ioutil.ReadAll(output.Body)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func main() {
+
+	b := make([]byte, 1024)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	key := "keyname"
+	err = send(key, b)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	b2, err := recv(key)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if testEq(b, b2) {
+		fmt.Println("Worky")
+	}
+
+}
